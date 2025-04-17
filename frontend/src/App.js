8f091c46686248862b561e8212250c654d753e51
Vikas Shejul <vikasshejul591@gmail.com>
2025-04-17 21:39:49 +0530

    My first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Loginpag from './jsxcss/Loginpag.jsx';
+
+function App() {
+  return (
+    <div>
+    <Loginpag />
+    </div>
+  );
+}
+
+export default App;
+
